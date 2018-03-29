55b02914c547b37a1793faa1879d9bb44675bee6
dlsrb6342 <dlsrb6342@gmail.com>
2018-03-29 16:23:26 +0900

    Add static file path

--- server/src/index.js
+++ server/src/index.js
@@ -4,8 +4,10 @@ import path from 'path';
 const app = express();
 const port = 3000;
 
+app.use('/', express.static(path.join(__dirname, '../../client/dist')));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './../client/dist/index.html'));
+  res.sendFile(path.resolve(__dirname, '../../client/dist/index.html'));
 });
 
 app.listen(port, () => {
